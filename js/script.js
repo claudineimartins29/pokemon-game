a98a2f2e19e2ea1d86ae14e0dd9b8f57f1888033
Claudinei Martins <claudineimartins29@gmail.com>
2024-05-22 21:59:06 -0300

    movimento pokemons

---
+++ js/script.js
@@ -0,0 +1,68 @@
+// . classes
+// # ids
+// nome do elemento
+// div h1=> elemento dento de outro elemento
+
+//querySelector => seleciona apenas um elemento (o primeiro q ele encontrar)
+//query SelectorAll => seleciona todos os elementos e coloca dentro de um array
+
+const body = document.querySelector("body");
+const game = document.querySelector(".game");
+
+const count = document.querySelector("h1");
+const reset = document.querySelector(".reset");
+
+const ash = document.querySelector("#ash");
+const charmander = document.querySelector("#charmander");
+const pikachu = document.querySelector("#pikachu");
+const zubat = document.querySelector("#zubat");
+
+function getRightPosition(){
+    return parseInt(ash.style.right.split("px")) || 2;
+}
+
+function getTopPosition(){
+    return parseInt(ash.style.top.split("px")) || 2;
+}
+
+body.addEventListener("keydown", (event) => {
+    event.stopPropagation();
+
+    switch (event.code) {
+        case "ArrowLeft":
+          if (getRightPosition() < 770) {
+            ash.style.right = `${getRightPosition()+8}px`
+            ash.src = "../assets/left.png"
+          }  
+        break;
+        
+        case 'ArrowRight':
+            if (getRightPosition() > 2) {
+                ash.style.right = `${getRightPosition()-8}px`
+                    ash.src = "../assets/right.png"
+            }  
+
+        break;
+
+        case 'ArrowDown':
+            if (getTopPosition() < 625) {
+                ash.style.top = `${getTopPosition()+8}px`
+                ash.src = "../assets/front.png"
+            }  
+
+        break;
+
+        case 'ArrowUp':
+            if (getTopPosition() > 2) {
+                ash.style.top = `${getTopPosition()-8}px`
+                ash.src = "../assets/back.png"
+            }  
+
+
+        break;
+    
+        default:
+            break;
+    }
+
+})
